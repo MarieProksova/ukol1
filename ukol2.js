668b7d22b807031616814ef10c2797196912c216
MarieProksova <marie.milatova@gmail.com>
2021-10-09 13:00:21 +0200

    1 file modified

--- ukol2.js
+++ ukol2.js
@@ -4,8 +4,8 @@ let vek = Number(prompt ("Zadej věk"));
  
 if (vek < 18) {
   let vysledek = 18 - vek;
-  console.log ("Vaše jméno je" + jmeno + ", je Vám " + vek + "let a tak musíte ještě "+ vysledek + " let počkat, než budete moci řídit auto");
+  console.log ("Vaše jméno je" + jmeno + ", je Vám " + vek + "let a tak musíte ještě "+ vysledek + " let počkat, než budete moci řídit auto.");
 }
   else {
-    console.log("Vaše jméno je" + jmeno + ", je Vám " + vek + "let a tak máte právo řídit auto")
+    console.log("Vaše jméno je " + jmeno + ", je Vám " + vek + " let a tak máte právo řídit auto")
   };
